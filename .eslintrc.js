46926144920fc04e262afc3a92cfa372dcbb4a28
Alexander von Studnitz <alexander.von.studnitz@gmail.com>
2019-07-25 13:55:07 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  extends: ['plugin:vue/recommended', 'prettier', 'prettier/vue'],
+  // required to lint *.vue files
+  plugins: ['vue', 'prettier'],
+  // add your custom rules here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+        },
+      },
+    ],
+    'prettier/prettier': 'error',
+  },
+}
